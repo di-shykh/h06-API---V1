ab73879712788c88ed31a2adac2091d25b70c76d
di-shykh <di49@mail.ru>
2026-01-08 23:09:57 +0300

    feat: start to get comments list

--- src/comments/repositories/comments.query-repository.ts
+++ src/comments/repositories/comments.query-repository.ts
@@ -4,6 +4,7 @@ import {commentCollection, userCollection} from "../../db/mongo.bd";
 import {RepositoryNotFoundError} from "../../core/errors/repository-not-found.error";
 import {CommentOutput} from "../routes/output/comment-output";
 import {UserDB} from "../../users/routes/output/user.db";
+import {CommentQueryInput} from "../routes/input/comment-query.input";
 
 export const commentsQueryRepository = {
     async findCommentById(id: string): Promise<WithId<CommentDB>> {
@@ -12,6 +13,27 @@ export const commentsQueryRepository = {
             throw new RepositoryNotFoundError("Comment not found.");
         }
         return result;
+    },
+    async findManyComments(queryDto: CommentQueryInput): Promise<{items: WithId<Comment>[], totalCount: number}> {
+        const {
+            pageNumber,
+            pageSize,
+            sortBy,
+            sortDirection,
+            searchContentTerm,
+        } = queryDto;
+        const skip = (pageNumber - 1) * pageSize;
+        const filter: any = {};
+        if (searchContentTerm) {
+            filter.content = { $regex: searchContentTerm, $options: "i" };
+        }
+        const items: WithId<Comment[]> = await commentCollection
+            .find(filter)
+            .sort({[sortBy]: sortDirection})
+            .skip(skip)
+            .limit(pageSize)
+            .toArray()
+        ;
     },
     async mapToCommentOutput(comment: WithId<CommentDB>): Promise<CommentOutput> {
         const user: WithId<UserDB> | null = await userCollection.findOne({_id: new ObjectId(comment.userId)});
@@ -28,6 +50,9 @@ export const commentsQueryRepository = {
             createdAt: comment.createdAt,
         }
         return commentOutput;
+    },
+    async mapToCommentListOutput() {
+
     }
 }
 
--- src/comments/routes/handlers/get-comment-list.handler.ts
+++ src/comments/routes/handlers/get-comment-list.handler.ts
@@ -1,9 +1,25 @@
 import {Request, Response} from "express";
 import {errorHandler} from "../../../core/errors/error.handler";
+import {CommentQueryInput} from "../input/comment-query.input";
+import {matchedData} from "express-validator";
+import {setDefaultSortAndPaginationIfNotExist} from "../../../core/helpers/set-default-sort-and-pagination";
+import {commentsQueryRepository} from "../../repositories/comments.query-repository";
+import {CommentListPaginatedOutput} from "../output/comment-list-paginated.output";
 
 export async function getCommentListHandler(req: Request, res: Response) {
     try{
-        const query = req.query as CommentQueryInput;
+        const query = req.query as unknown as CommentQueryInput;
+        const sanitizedQuery = matchedData<CommentQueryInput>(req, {
+            locations: ['query'],
+            includeOptionals: true,
+        });
+        const queryInput = setDefaultSortAndPaginationIfNotExist(sanitizedQuery);
+        const {items, totalCount} = await commentsQueryRepository.findManyComments(queryInput);
+        const commentsListOutput: CommentListPaginatedOutput = commentsQueryRepository.mapToCommentListOutput(items,
+            queryInput.pageNumber,
+            queryInput.pageSize,
+            totalCount,
+        )
     } catch (e) {
         errorHandler(e, res);
     }
